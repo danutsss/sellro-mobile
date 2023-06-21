3619d9b0f381f98708066a5bfa54dede1318da84
danut <matei1337@icloud.com>
2023-06-21 15:07:29 +0300

    post interface, api fetch function, solved some errors

---
+++ src/composables/api.ts
@@ -0,0 +1,21 @@
+import { Post } from "@/interfaces/post";
+
+export const fetchData = async (
+    apiUrl: string,
+    options: object
+): Promise<Post> => {
+    try {
+        const response = await fetch(apiUrl, options);
+
+        if (!response.ok) {
+            throw new Error(
+                `Error fetching data from ${apiUrl}. Status: ${response.status}`
+            );
+        }
+
+        const data: Post = await response.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(`Error fetching data from ${apiUrl}. ${error.message}`);
+    }
+};
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const API_URL = "http://sell.ro";
+export const API_ENDPOINT_AUTH = "api/auth";
+export const API_ENDPOINT_POSTS = "api/posts";
+export const API_ENDPOINT_POST = "api/post";
