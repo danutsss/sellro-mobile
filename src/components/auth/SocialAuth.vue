467d2bc574275eb4d442d24f94e3c1024f19bc44
danut <matei1337@icloud.com>
2023-07-10 15:58:45 +0300

    social auth buttons component

---
+++ src/components/auth/SocialAuth.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col p-3">
+        <ion-button class="font-bold" type="submit" color="dark">
+            <ion-icon
+                class="mr-1 text-blue-500"
+                slot="icon-only"
+                :icon="logoFacebook"></ion-icon>
+            Continua cu Facebook
+        </ion-button>
+        <ion-button class="font-bold" type="submit" color="dark">
+            <ion-icon
+                class="mr-1 text-red-500"
+                slot="icon-only"
+                :icon="logoGoogle"></ion-icon>
+            Continua cu Google
+        </ion-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { IonButton, IonIcon } from "@ionic/vue";
+import { logoFacebook, logoGoogle } from "ionicons/icons";
+</script>
