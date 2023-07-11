01e1e96c1ad92a4d990679d871b2b1d8bda2fe29
danut <matei1337@icloud.com>
2023-07-11 16:32:30 +0300

    new methods for api

--- src/composables/api.ts
+++ src/composables/api.ts
@@ -21,6 +21,46 @@ export const fetchPost = async (
     }
 };
 
+export const fetchUserPost = async (
+    apiUrl: URL,
+    options: object
+): Promise<Post> => {
+    try {
+        const response = await fetch(apiUrl, options);
+
+        if (!response.ok) {
+            throw new Error(
+                `Error fetching data from ${apiUrl}. Status: ${response.status}`
+            );
+        }
+
+        const data: Post = await response.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(`Error fetching data from ${apiUrl}. ${error.message}`);
+    }
+};
+
+export const getCityById = async (
+    apiUrl: string,
+    options: object
+): Promise<any> => {
+    try {
+        const response = await fetch(apiUrl, options);
+
+        if (!response.ok) {
+            throw new Error(
+                `Error fetching data from ${apiUrl}. Status: ${response.status}`
+            );
+        }
+
+        const data: Post = await response.json();
+        return data;
+    } catch (error: any) {
+        throw new Error(`Error fetching data from ${apiUrl}. ${error.message}`);
+    }
+};
+
 export const loginUser = async (
     apiUrl: string,
     options: object
