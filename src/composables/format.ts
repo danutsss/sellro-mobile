6d55f1ce3eed771a79aea37d5567656e45fb1c78
danut <matei1337@icloud.com>
2023-07-12 16:00:03 +0300

    added a new function that formats dates to romanian date time format, removed fraction digits from listing price

--- src/composables/format.ts
+++ src/composables/format.ts
@@ -1,6 +1,16 @@
 export const formatPrice = (price: number) => {
     return new Intl.NumberFormat("ro-RO", {
         style: "currency",
-        currency: "RON",
-    }).format(price);
+        currency: "lei",
+        minimumFractionDigits: 0,
+    })
+        .format(price)
+        .toLocaleLowerCase();
+};
+
+export const formatDate = (date: string) => {
+    return new Intl.DateTimeFormat("ro-RO", {
+        dateStyle: "medium",
+        timeStyle: "short",
+    }).format(new Date(date));
 };
