5855947a5a62105a2eee088b515e0d94d1c54fe8
danut <matei1337@icloud.com>
2023-07-21 16:53:50 +0300

    replaced sharebuttons component with @capacitor/share plugin

--- src/components/listing/layout/Content.vue
+++ src/components/listing/layout/Content.vue
@@ -42,8 +42,14 @@
             <h1 class="font-sans text-2xl mt-2">
                 {{ listing.result.title }}
             </h1>
-            <h1 class="font-bold font-sans text-3xl">
+            <h1 class="font-bold font-sans text-3xl flex items-center">
                 {{ formatPrice(listing.result.price) }}
+
+                <small
+                    v-if="listing.result.negotiable"
+                    class="ml-1 text-xs uppercase bg-[#2ecc71] p-[0.15rem] rounded">
+                    Negociabil
+                </small>
             </h1>
         </div>
     </div>
@@ -94,13 +100,22 @@
             {{ listing.result.description }}
         </p>
     </div>
+
     <HorizontalLine />
 
-    <ShareButtons />
+    <div id="shareListing" class="p-3">
+        <ion-button @click="share" color="primary" class="w-full">
+            Distribuie anuntul
+        </ion-button>
+    </div>
+
+    <HorizontalLine />
+
+    <Footer />
 </template>
 
 <script setup lang="ts">
-import { IonIcon, IonChip, IonLabel } from "@ionic/vue";
+import { IonIcon, IonChip, IonLabel, IonButton } from "@ionic/vue";
 import { request } from "@/composables/api";
 import { ref } from "vue";
 import { API_ENDPOINT_POSTS, API_URL } from "@/constants";
@@ -111,9 +126,10 @@ import { Pagination } from "swiper/modules";
 import { formatDate, formatPrice } from "@/composables/format";
 import { useAuthStore } from "@/store/authStore";
 import { folderOpen, time, location, eye } from "ionicons/icons";
+import { Share } from "@capacitor/share";
 
 import HorizontalLine from "@/components/layout/HorizontalLine.vue";
-import ShareButtons from "@/components/layout/ShareButtons.vue";
+import Footer from "./Footer.vue";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -167,4 +183,19 @@ const archiveListing = async (listingId: number) => {
         archiveOptions
     );
 };
+
+const share = async () => {
+    await Share.share({
+        title: `${listing.result.title} - ${listing.result.price} RON`,
+        text: listing.result.description,
+        url: listing.result.url,
+        dialogTitle: `Distribuie anuntul ${listing.result.title}`,
+    });
+};
 </script>
+
+<style scoped>
+ion-chip {
+    cursor: auto;
+}
+</style>
