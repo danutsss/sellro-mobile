9b7e0db5d1e10d08cb14fc49eb52865201332766
danut <matei1337@icloud.com>
2023-07-22 04:24:59 +0300

    two new endpoints to constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,5 @@
 export const API_URL = "http://sell.ro";
 export const API_ENDPOINT_AUTH = "api/auth";
 export const API_ENDPOINT_POSTS = "api/posts";
+export const API_ENDPOINT_CATEGORIES = "api/categories";
+export const API_ENDPOINT_HOMESECTIONS = "api/homeSections";
