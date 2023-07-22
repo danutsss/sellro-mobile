59c2e2215edd981febed85592784f7361c786224
danut <matei1337@icloud.com>
2023-07-22 04:29:43 +0300

    removed useless class from horizontal line component

--- src/components/layout/HorizontalLine.vue
+++ src/components/layout/HorizontalLine.vue
@@ -1,5 +1,5 @@
 <template>
     <div class="flex items-center justify-center px-3">
-        <div class="border-t border-gray-300 opacity-25 w-full"></div>
+        <div class="border-t opacity-25 w-full"></div>
     </div>
 </template>
